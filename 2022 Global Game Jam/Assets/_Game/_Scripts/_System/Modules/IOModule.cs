a9aafec0dd54b4855a83f3c60b44b92ea9d47045
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2022-01-28 22:20:24 +0000

    IOModule

---
+++ 2022 Global Game Jam/Assets/_Game/_Scripts/_System/Modules/IOModule.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace blu
+{
+    public class IOModule : Module
+    {
+
+        const string filename = "savedata.blu";
+        const string listKey = "LevelsComplete";
+        Dictionary<int, bool> levelsComplete;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Read();
+        }
+
+        private void OnEnable()
+        {
+            base.Awake();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public bool IsLevelCompleted(int level)
+        {
+            if(levelsComplete.ContainsKey(level))
+            {
+                return levelsComplete[level];
+            }
+
+            return false;
+        }
+
+        public void SetLevelComplete(int level, bool complete)
+        {
+            if (levelsComplete.ContainsKey(level))
+            {
+                levelsComplete[level] = complete;
+            }
+            else
+            {
+                levelsComplete.Add(level, complete);
+            }
+
+            Write();
+        }
+
+        private void Read()
+        {
+            ES3.Load(listKey, filename, levelsComplete);
+        }
+
+        private void Write()
+        {
+            ES3.Save(listKey, levelsComplete, filename);
+        }
+
+    }
+}
